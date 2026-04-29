41d0fe1b839d4ad812064b7c864b901fa82b3049
Lithium <135757344+funhaji@users.noreply.github.com>
2026-04-30 01:15:46 +0330

    Update telegram.ts

--- api/telegram.ts
+++ api/telegram.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { handleTelegramUpdate } from "../lib/bot.js";
+import { ensureSchema } from "../lib/db.js";
 import { tg } from "../lib/telegram.js";
 import { logError } from "../lib/log.js";
 
@@ -9,6 +10,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     return;
   }
   try {
+    await ensureSchema();
     await handleTelegramUpdate(req.body);
     res.status(200).json({ ok: true });
   } catch (error) {
